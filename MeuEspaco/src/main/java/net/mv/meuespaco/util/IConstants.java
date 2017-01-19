22ecb79645c24cffb09bd6e77dda7809062379c8
Sidronio Lima <sidronio@sidroniolima.com.br>
2017-01-19 17:37:13 -0200

    Alteração do nome do arquivo de integração.

--- MeuEspaco/src/main/java/net/mv/meuespaco/util/IConstants.java
+++ MeuEspaco/src/main/java/net/mv/meuespaco/util/IConstants.java
@@ -15,7 +15,7 @@ public interface IConstants {
 	int QTD_PADRAO_REGISTROS_POR_PAGINA = 40;
 	String CAMINHO_DAS_FOTOS = "/home/meuvisua/var/static/images/pecas/";
 	String CAMINHO_DO_ARQUIVO_DE_EXPORTACAO = "http://meuvisualsemijoias.com/integracao/";
-	String NOME_DO_ARQUIVO_DE_EXPORTACAO = "Integracao.csv";
+	String NOME_DO_ARQUIVO_DE_EXPORTACAO = "integracao.csv";
 	String PRODUTO_SEM_FOTO = "Produto_sem_foto.png";
 	int QTD_EXIBIDA_NA_LISTAGEM_DE_PRODUTOS = 21;
 	String CAMINHO_DOS_RELATORIOS = "http://meuvisualsemijoias.com/relatorios/rel-escolha.jrxml";
--- MeuEspaco/src/test/java/net/mv/meuespaco/util/ParseFromCsvTest.java
+++ MeuEspaco/src/test/java/net/mv/meuespaco/util/ParseFromCsvTest.java
@@ -1,6 +1,8 @@
 package net.mv.meuespaco.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.HashMap;
